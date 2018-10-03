137c584a51ea0310b0888f392a11619b06e86b37
mindoiudorin <mindoiu.dorin@gmail.com>
2018-10-03 11:10:58 +0300

    Add files via upload

---
+++ AccountManager/App_Start/BundleConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AccountManager
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/bower_components/toastr/toastr.min.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js",
+                        "~/Scripts/app.shared.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*",
+                        "~/bower_components/webshim/js-webshim/minified/polyfiller.js",
+                        "~/bower_components/combodate/src/combodate.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/bower_components/bootstrap/dist/js/bootstrap.min.js",
+                      "~/Scripts/respond.js",
+                        "~/bower_components/moment/moment.js",
+                        "~/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/bower_components/bootstrap/dist/css/bootstrap.min.css",
+                      "~/Content/site.css",
+                      "~/Content/app.layout.css",
+                      "~/bower_components/toastr/toastr.min.css",
+                      "~/bower_components/font-awesome/css/font-awesome.min.css",
+                      "~/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css"));
+        }
+    }
+}
---
+++ AccountManager/App_Start/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountManager.App_Start
+{
+    public static class WebHelpers
+    {
+        public static string CurrentAction(this UrlHelper urlHelper)
+        {
+            var routeValueDictionary = urlHelper.RequestContext.RouteData.Values;
+            // in case using virtual dirctory 
+            var rootUrl = urlHelper.Content("~/");
+            return string.Format("{0}{1}/{2}/", rootUrl, routeValueDictionary["controller"], routeValueDictionary["action"]);
+        }
+
+        public static string CurrentController(this UrlHelper urlHelper)
+        {
+            var routeValueDictionary = urlHelper.RequestContext.RouteData.Values;
+            // in case using virtual dirctory 
+            var rootUrl = urlHelper.Content("~/");
+            return string.Format("{0}{1}/", rootUrl, routeValueDictionary["controller"]);
+        }
+
+        public static string RootUrl(this UrlHelper urlHelper)
+        {
+            // in case using virtual dirctory 
+            return urlHelper.Content("~/");
+        }
+    }
+}
